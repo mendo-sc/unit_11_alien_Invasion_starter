4533b11eb8313b32fc7af83147fee6f0c1513a3a
mendo-sc <mendo@student.cscc.edu>
2025-04-11 11:32:52 -0400

    Fleet also creates all columns

--- alien_fleet.py
+++ alien_fleet.py
@@ -18,29 +18,40 @@ class AlienFleet:
     
     def create_fleet(self) -> None:
         alien_h = self.settings.alien_h
+        alien_w = self.settings.alien_w
         screen_h = self.settings.screen_h
+        screen_w = self.settings.screen_w
 
-        fleet_h = self.calculate_fleet_size(alien_h, screen_h)
+        fleet_h, fleet_w = self.calculate_fleet_size(alien_h, screen_h, alien_w, screen_w)
 
-        # half_screen = self.settings.screen_h
+        half_screen = self.settings.screen_h
         fleet_vertical_space = fleet_h * alien_h
+        fleet_horizontal_space = fleet_w * alien_w
         y_offset = int((screen_h - fleet_vertical_space)//2)
+        x_offset = int((half_screen - fleet_horizontal_space)//2)
+        for col in range(fleet_w):
+            for row in range(fleet_h):
+                current_y = alien_h * row + y_offset
+                current_x = alien_w * col + x_offset
+                if row % 2 == 0 or col % 2 == 0:
+                    continue
+                self._create_alien(current_y, current_x)
 
-        for row in range(fleet_h):
-            current_y = alien_h * row + y_offset
-            if row % 2 == 0:
-                continue
-            self._create_alien(current_y, 10)
-
-    def calculate_fleet_size(self, alien_h, screen_h) -> any:
+    def calculate_fleet_size(self, alien_h, screen_h, alien_w, screen_w) -> any:
         fleet_h = (screen_h // alien_h)
+        fleet_w = ((screen_w / 2) // alien_w)
 
         if fleet_h % 2 == 0:
             fleet_h -= 1
         else:
             fleet_h -= 2
 
-        return fleet_h
+        if fleet_w % 2 == 0:
+            fleet_w -= 1
+        else:
+            fleet_w -= 2
+
+        return int(fleet_h), int(fleet_w)
     
     def _create_alien(self, current_y: int, current_x: int):
         new_alien = Alien(self, current_y, current_x)
