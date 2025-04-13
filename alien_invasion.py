34a64969ca1c89894b42f4ed904e4ac203ab2bc0
mendo-sc <mendo@student.cscc.edu>
2025-04-13 09:33:28 -0400

    Set up ship to reset when colliding with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,8 @@ class AlienInvasion:
         pygame.mixer.init()
         self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
         self.laser_sound.set_volume(0.2)
+        self.impact_sound = pygame.mixer.Sound(self.settings.impact_sound)
+        self.impact_sound.set_volume(0.2)
 
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
@@ -46,8 +48,21 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.alien_fleet.update_fleet()
+            self._check_collisions()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _check_collisions(self) -> None:
+        """ Checks for collisions """
+        if self.ship.check_collisions(self.alien_fleet.fleet):
+            self._reset_level()
+        
+        
+
+    def _reset_level(self) -> None:
+        self.ship.arsenal.arsenal.empty()
+        self.alien_fleet.fleet.empty()
+        self.alien_fleet.create_fleet()
 
     def _update_screen(self) -> None:
         """ Draws and updates screen """
--- ship.py
+++ ship.py
@@ -21,11 +21,14 @@ class Ship:
         self.image = pygame.transform.rotate(self.image, 90)
         
         self.rect = self.image.get_rect()
-        self.rect.midright = self.boundaries.midright
+        self._center_ship()
         self.moving_up = False
         self.moving_down = False
-        self.y = float(self.rect.y)
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midright = self.boundaries.midright
+        self.y = float(self.rect.y)
     
     def update(self):
         """ Update ship position and arsenal """
@@ -55,3 +58,9 @@ class Ship:
             False: if bullet cannot be fired
         """
         return self.arsenal.fire_bullet()
+
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
