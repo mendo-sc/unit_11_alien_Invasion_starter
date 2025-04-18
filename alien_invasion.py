3d0be8424d7259c28dc9969bdf6d2b4791c0eaa2
mendo-sc <mendo@student.cscc.edu>
2025-04-18 16:20:37 -0400

    Finished HUD life and level stat display

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,7 @@ class AlienInvasion:
             self._reset_level()
             self.settings.increase_difficulty()
             self.game_stats.update_level()
+            self.HUD.update_level()
 
     def _check_game_status(self) -> None:
         if self.game_stats.ships_left > 0:
--- hud.py
+++ hud.py
@@ -17,8 +17,15 @@ class HUD:
         )
         self.padding = 20
         self.update_scores()
-        # self.setup_life_image()
-        # self.update_level()
+        self._setup_life_image()
+        self.update_level()
+
+    def _setup_life_image(self) -> None:
+        self.life_image = pygame.image.load(self.settings.ship_file)
+        self.life_image = pygame.transform.scale(self.life_image, (
+            self.settings.ship_w, self.settings.ship_h
+        ))
+        self.life_rect = self.life_image.get_rect()
 
     def update_scores(self) -> None:
         self._update_max_score()
@@ -51,7 +58,25 @@ class HUD:
         self.hi_score_rect = self.score_image.get_rect()
         self.hi_score_rect.midtop = (self.boundaries.centerx, self.padding)
 
-    def draw(self):
+    def update_level(self) -> None:
+        level_str = f'level: {self.game_stats.level: ,.0f}'
+        self.level_image = self.font.render(
+            level_str, True, self.settings.text_color, None
+        )
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.left = self.padding
+        self.level_rect.top = self.life_rect.bottom + self.padding
+
+    def _draw_lives(self) -> None:
+        current_x = self.padding
+        current_y = self.padding
+        for _ in range(self.game_stats.ships_left):
+            self.screen.blit(self.life_image, (current_x, current_y))
+            current_x += self.life_rect.width + self.padding
+
+    def draw(self) -> None:
         self.screen.blit(self.max_score_image, self.max_score_rect)
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.hi_score_image, self.hi_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self._draw_lives()
